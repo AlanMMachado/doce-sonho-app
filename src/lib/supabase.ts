2e8c6020da29635c58c584ebf1f21787e64cdc71
AlanMMachado <sp.alanmarques@gmail.com>
2026-06-27 16:29:49 -0300

    feat(supabase): implementar LocalStorage para visualização web

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,20 @@
-import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
+// AsyncStorage para o web (LocalStorage)
+const webStorage = {
+  getItem: (key: string) => Promise.resolve(typeof window !== 'undefined' ? window.localStorage.getItem(key) : null),
+  setItem: (key: string, value: string) => { if (typeof window !== 'undefined') window.localStorage.setItem(key, value); return Promise.resolve(); },
+  removeItem: (key: string) => { if (typeof window !== 'undefined') window.localStorage.removeItem(key); return Promise.resolve(); },
+};
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
-        storage: AsyncStorage,
+        storage: Platform.OS === 'web' ? webStorage : AsyncStorage,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
